b67ee84db8aaaeeaca116f4e6106d7924732689d
Y_ALANG <92966060+yalwolf@users.noreply.github.com>
2022-01-15 16:10:42 +0800

    Update panurl.js

--- panurl.js
+++ panurl.js
@@ -1,16 +1,16 @@
 // ==UserScript==
 // @name              网盘智能识别助手Plus
 // @namespace         https://github.com/yalwolf/panAI
-// @version           2.0
+// @version           2.0.5
 // @author            一只阿狼哒&YouXiaoHou
-// @icon              https://www.youxiaohou.com/panai.png
-// @icon64            https://www.youxiaohou.com/panai.png
-// @description       AI智能识别选中文字中的【网盘链接】和【提取码】，识别成功打开网盘链接并自动填写提取码，省去手动复制提取码在输入的烦恼。支持百度网盘，腾讯微云，蓝奏云，天翼云，和彩云，迅雷云盘，123云盘。
+// @icon              https://js.alwolf.cn/images/panurl.png
+// @icon64            https://js.alwolf.cn/images/panurl.png
+// @description       AI智能识别选中文字中的【网盘链接】和【提取码】，识别成功打开网盘链接并自动填写提取码，省去手动复制提取码在输入的烦恼。支持百度网盘、腾讯微云、蓝奏云、天翼云、和彩云、迅雷云盘、123云盘、Google云、坚果云、360云盘。
 // @license           AGPL
-// @homepage          https://www.youxiaohou.com/tool/install-panai.html
-// @supportURL        https://github.com/syhyz1990/panAI
-// @updateURL
-// @downloadURL
+// @homepage          https://js.alwolf.cn/
+// @supportURL        https://github.com/yalwolf/panAI
+// @updateURL         https://js.alwolf.cn/js/panurl.js
+// @downloadURL       https://js.alwolf.cn/js/panurl.js
 // @match             *://*/*
 // @require           https://unpkg.com/sweetalert2@10.16.6/dist/sweetalert2.min.js
 // @resource          swalStyle https://unpkg.com/sweetalert2@10.16.6/dist/sweetalert2.min.css
@@ -50,7 +50,7 @@
 
     let util = {
         clog(c) {
-            console.group('[网盘智能识别助手]');
+            console.group('[网盘智能识别助手Plus]');
             console.log(c);
             console.groupEnd();
         },
@@ -177,6 +177,14 @@
             input: ['#accessCode'],
             button: ['#submitBtn'],
             name: '坚果云',
+            storage: 'hash'
+        },
+        '360pan': {
+            reg: /((?:https?:\/\/)?yunpan\.360\.cn\/[A-Za-z0-9_\-]+)/,
+            host: /yunpan\.360\.cn/,
+            input: ['#accessCode'],
+            button: ['#submitBtn'],
+            name: '360云盘',
             storage: 'hash'
         },
         google: {
@@ -423,6 +431,7 @@
                               class="panai-setting-checkbox"></label>
                               <label class="panai-setting-label">倒计时结束自动打开<input type="checkbox" id="S-Timer-Open" ${util.getValue('setting_timer_open') ? 'checked' : ''} class="panai-setting-checkbox"></label>
                               <label class="panai-setting-label"><span>倒计时 <span id="Timer-Value">（${util.getValue('setting_timer') / 1000}秒）</span></span><input type="range" id="S-Timer" min="500" max="10000" step="500" value="${util.getValue('setting_timer')}" style="width: 200px;"></label>
+							   <label class="panai-setting-label"><a href="https://js.alwolf.cn/tutorial/panurl"title="脚本丨教程" target="_blank" class="links" style="text-decoration:none;">使用教程</label>
                             </div>`;
                 Swal.fire({
                     title: '识别助手配置',
@@ -430,7 +439,7 @@
                     icon: 'info',
                     showCloseButton: true,
                     confirmButtonText: '保存',
-                    footer: '<div style="text-align: center;font-size: 1em;">点击查看 <a href="https://www.youxiaohou.com/tool/install-panai.html" target="_blank">使用说明</a>，助手免费开源，<a href="https://www.youxiaohou.com/tool/install-panai.html">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
+                    footer: '<div style="text-align: center;font-size: 1em;">点击查看 <a href="https://js.alwolf.cn/js/updates/panurl">检查更新</a><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path d="M445.956 138.812L240.916 493.9c-11.329 19.528-12.066 44.214 0 65.123 12.067 20.909 33.898 32.607 56.465 32.607h89.716v275.044c0 31.963 25.976 57.938 57.938 57.938h134.022c32.055 0 57.938-25.975 57.938-57.938V591.63h83.453c24.685 0 48.634-12.803 61.806-35.739 13.172-22.844 12.343-50.016 0-71.386l-199.42-345.693c-13.633-23.58-39.24-39.516-68.44-39.516-29.198 0-54.897 15.935-68.438 39.516z" fill="#d81e06"/></svg></div>',
                     customClass
                 }).then((res) => {
                     res.isConfirmed && history.go(0);
